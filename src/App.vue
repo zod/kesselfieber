b0dd9fbe10f81be0720b96135e07c3f45160e90f
Manuel Fuhr <manuel@knid.net>
2020-05-09 17:41:17 +0200

    WIP Vue

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Segment 1 Zeit</th>
+          <th>Segment 1 Platz</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>KOM</th>
+          <td>3:57</td>
+          <td>Simon B.</td>
+        </tr>
+        <tr>
+          <th>Manuel F.</th>
+          <td v-for="item in leaderboards">{{ item }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import leaderboards from "../test/leaderboards.json";
+
+console.log(leaderboards);
+
+export default {
+  name: "app",
+  data() {
+    return {
+      leaderboards: leaderboards
+    };
+  }
+};
+</script>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
