ff689ed5bc295dba58dfb612e7a70a4bc79cd860
Manuel Fuhr <manuel@knid.net>
2020-05-09 11:51:05 +0200

    Use vue-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 }
